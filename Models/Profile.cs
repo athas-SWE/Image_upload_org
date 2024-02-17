d787abb87bd7f40e3d69d66ecfb96f1ba622aa8f
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-17 15:52:48 +0530

    first

---
+++ Models/Profile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Image_upload.Models
+{
+    public partial class Profile
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ Models/imageFileContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Image_upload.Models
+{
+    public partial class imageFileContext : DbContext
+    {
+        public imageFileContext()
+        {
+        }
+
+        public imageFileContext(DbContextOptions<imageFileContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Profile> Profiles { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=;Database=imageFile;Trusted_Connection=True;Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profile>(entity =>
+            {
+                entity.ToTable("profile");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Image).HasColumnName("image");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
